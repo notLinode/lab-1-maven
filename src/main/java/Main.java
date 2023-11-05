8a59d84c38e7ddba9838fd3b1cfe8a6be17c9413
Iskenderov Andrei <linode@duck.com>
2023-11-05 11:16:58 +0300

    Организовал выполнение всех заданий подряд

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
-        // Стоило ли мне дать им нормальные имена?..
+        Scanner sc = new Scanner(System.in);
+
         Exercise ex1 = new Exercise1();
         Exercise ex2 = new Exercise2();
         Exercise ex3 = new Exercise3();
@@ -16,54 +16,8 @@ public class Main {
         Exercise ex8 = new Exercise8();
         Exercise ex9 = new Exercise9();
 
-        System.out.print("Введите номер задания 1-9 (0 для выхода):\n> ");
-
-        for (int input = sc.nextInt(); input != 0; input = sc.nextInt()) {
-            sc.nextLine();
-
-            switch (input) {
-                case 1:
-                    ex1.solve(sc);
-                    break;
-
-                case 2:
-                    ex2.solve(sc);
-                    break;
-
-                case 3:
-                    ex3.solve(sc);
-                    break;
-
-                case 4:
-                    ex4.solve(sc);
-                    break;
-
-                case 5:
-                    ex5.solve(sc);
-                    break;
-
-                case 6:
-                    ex6.solve(sc);
-                    break;
-
-                case 7:
-                    ex7.solve(sc);
-                    break;
-
-                case 8:
-                    ex8.solve(sc);
-                    break;
-
-                case 9:
-                    ex9.solve(sc);
-                    break;
-
-                default:
-                    break;
-            }
-
-            System.out.print("Введите номер задания 1-9 (0 для выхода):\n> ");
-        }
+        List<Exercise> exList = List.of( ex1, ex2, ex3, ex4, ex5, ex6, ex7, ex8, ex9 );
+        exList.forEach(ex -> ex.solve(sc));
     }
 
 }
